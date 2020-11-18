17857b4f765ffd5fb1de8c1515688a370d72f5e5
kbGiff <uaiporn.p@giffarinefactor.com>
2020-11-18 15:54:07 +0700

    Day 2 Finish 18Nov

--- lib/state/my_service.dart
+++ lib/state/my_service.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-
-
+import 'package:kbgiffarine/state/authen.dart';
 
 class MyService extends StatefulWidget {
   @override
@@ -12,7 +12,46 @@ class _MyServiceState extends State<MyService> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(),
+      drawer: buildDrawer(),
     );
   }
+
+  Drawer buildDrawer() => Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.red.shade700),
+              child: ListTile(
+                onTap: () async {
+                  await Firebase.initializeApp().then((value) async {
+                    await FirebaseAuth.instance.signOut().then(
+                          (value) => Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Authen(),
+                              ),
+                              (route) => false),
+                        );
+                  });
+                },
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.white,
+                  size: 36,
+                ),
+                title: Text(
+                  'Sign Out',
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  'คือการออกจาก Account เพื่อ Login ใหม่',
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 }
--- lib/state/register.dart
+++ lib/state/register.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kbgiffarine/models/user_model.dart';
 import 'package:kbgiffarine/utility/normal_dialog.dart';
 import 'package:location/location.dart';
 
@@ -155,7 +159,9 @@ class _RegisterState extends State<Register> {
                   password == null ||
                   password.isEmpty) {
                 normalDialog(context, 'กรุณากรอกทุกช่อง');
-              } else {}
+              } else {
+                registerAndInsertData();
+              }
             },
             icon: Icon(Icons.cloud_upload),
             label: Text('Register'),
@@ -163,5 +169,38 @@ class _RegisterState extends State<Register> {
         ),
       ],
     );
+  }
+
+  Future<Null> registerAndInsertData() async {
+    await Firebase.initializeApp().then((value) async {
+      print('#### Initial Success ####');
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: user, password: password)
+          .then((value) async {
+        print('Success Register');
+        await FirebaseAuth.instance.authStateChanges().listen((event) async {
+          String uid = event.uid;
+          print('uid = $uid');
+
+          UserModel model = UserModel(
+              name: name,
+              email: user,
+              password: password,
+              lat: lat.toString(),
+              lng: lng.toString());
+
+          Map<String, dynamic> data = model.toMap();
+          await FirebaseFirestore.instance
+              .collection('user')
+              .doc(uid)
+              .set(data)
+              .then(
+                (value) => Navigator.pop(context),
+              );
+        });
+      }).catchError((error) {
+        normalDialog(context, error.message);
+      });
+    });
   }
 }
