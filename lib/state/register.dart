deae9cc936b89bfa6b05b1e5c4dabc03004b6c46
kbGiff <uaiporn.p@giffarinefactor.com>
2020-11-18 10:01:06 +0700

    update

--- lib/state/register.dart
+++ lib/state/register.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:location/location.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -8,10 +7,118 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  double lat, lng;
+
+  @override
+  void initState(){
+    super.initState();
+    findLatLng();
+  }
+
+  Future<Null> findLatLng()async{
+    LocationData locationData = await findLocationData();
+    setState(() {
+      lat = locationData.latitude;
+      lng=locationData.longitude;
+      print('lat = $lat, lng = $lng');
+    });
+  }
+
+  Future<LocationData> findLocationData()async{
+    Location location=Location();
+    try {
+      return location.getLocation();
+    } catch (e) {
+return null;
+    }
+  }
+
+
+  Container buildName() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.face),
+          labelText: 'Name :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container buildUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_box),
+          labelText: 'User :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.lock),
+          labelText: 'Password :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('Register'),),
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                buildName(),
+                buildUser(),
+                buildPassword(),
+                Expanded(
+                  child: Container(
+                    color: Colors.grey,
+                    width: MediaQuery.of(context).size.width,
+                    child: Text('This is Map'),
+                  ),
+                )
+              ],
+            ),
+          ),
+          buildElevatedButton(),
+        ],
+      ),
+    );
+  }
+
+  Column buildElevatedButton() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.cloud_upload),
+            label: Text('Register'),
+          ),
+        ),
+      ],
     );
   }
 }
