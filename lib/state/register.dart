ca02c681902c1b7f029fad57481c39ce7f8da1cd
kbGiff <uaiporn.p@giffarinefactor.com>
2020-11-18 11:49:32 +0700

    Day 2 Launch

--- lib/state/register.dart
+++ lib/state/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kbgiffarine/utility/normal_dialog.dart';
 import 'package:location/location.dart';
 
 class Register extends StatefulWidget {
@@ -8,37 +10,38 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   double lat, lng;
+  String name, user, password;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     findLatLng();
   }
 
-  Future<Null> findLatLng()async{
+  Future<Null> findLatLng() async {
     LocationData locationData = await findLocationData();
     setState(() {
       lat = locationData.latitude;
-      lng=locationData.longitude;
+      lng = locationData.longitude;
       print('lat = $lat, lng = $lng');
     });
   }
 
-  Future<LocationData> findLocationData()async{
-    Location location=Location();
+  Future<LocationData> findLocationData() async {
+    Location location = Location();
     try {
       return location.getLocation();
     } catch (e) {
-return null;
+      return null;
     }
   }
-
 
   Container buildName() {
     return Container(
       margin: EdgeInsets.only(top: 16),
       width: 250,
       child: TextField(
+        onChanged: (value) => name = value.trim(),
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.face),
           labelText: 'Name :',
@@ -53,6 +56,7 @@ return null;
       margin: EdgeInsets.only(top: 16),
       width: 250,
       child: TextField(
+        onChanged: (value) => user = value.trim(),
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.account_box),
           labelText: 'User :',
@@ -67,6 +71,7 @@ return null;
       margin: EdgeInsets.only(top: 16),
       width: 250,
       child: TextField(
+        onChanged: (value) => password = value.trim(),
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.lock),
           labelText: 'Password :',
@@ -90,13 +95,7 @@ return null;
                 buildName(),
                 buildUser(),
                 buildPassword(),
-                Expanded(
-                  child: Container(
-                    color: Colors.grey,
-                    width: MediaQuery.of(context).size.width,
-                    child: Text('This is Map'),
-                  ),
-                )
+                buildMap(context)
               ],
             ),
           ),
@@ -106,6 +105,40 @@ return null;
     );
   }
 
+  Set<Marker> mySet() {
+    return <Marker>[
+      Marker(
+          markerId: MarkerId('idUser'),
+          position: LatLng(lat, lng),
+          infoWindow: InfoWindow(
+            title: 'คุณอยู่ที่นี่',
+            snippet: 'lat = $lat,lng = $lng',
+          )),
+    ].toSet();
+  }
+
+  Expanded buildMap(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.only(top: 32, left: 16, right: 16, bottom: 64),
+        width: MediaQuery.of(context).size.width,
+        child: lat == null
+            ? buildProgress()
+            : GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(lat, lng),
+                  zoom: 16,
+                ),
+                mapType: MapType.normal,
+                onMapCreated: (controller) {},
+                markers: mySet(),
+              ),
+      ),
+    );
+  }
+
+  Center buildProgress() => Center(child: CircularProgressIndicator());
+
   Column buildElevatedButton() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
@@ -113,7 +146,17 @@ return null;
         Container(
           width: MediaQuery.of(context).size.width,
           child: ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              print('name = $name, user = $user, password = $password');
+              if (name == null ||
+                  name.isEmpty ||
+                  user == null ||
+                  user.isEmpty ||
+                  password == null ||
+                  password.isEmpty) {
+                normalDialog(context, 'กรุณากรอกทุกช่อง');
+              } else {}
+            },
             icon: Icon(Icons.cloud_upload),
             label: Text('Register'),
           ),
